a6f7a339b445ba831140069436e3cda2b489fe33
caiomunhoz <caio.gmunhoz@gmail.com>
2025-09-28 13:02:46 -0300

    old dag deleted

--- scripts/utils.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def format_timestamp_to_file_name(timestamp):
-    return timestamp.strftime('%Y-%m-%d_%H-%M-%S')
-
-def parse_custom_timestamp(timestamp):
-    return datetime.strptime(timestamp, '%Y-%m-%d_%H-%M-%S')
